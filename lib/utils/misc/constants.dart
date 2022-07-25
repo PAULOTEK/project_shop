e2ea82d9ebb06782b19364ee76d4636888578144
PauloTek <pauloalexandre820@gmail.com>
2022-07-25 19:21:15 -0300

    Implementação do modo web

--- lib/utils/misc/constants.dart
+++ lib/utils/misc/constants.dart
@@ -1,6 +1,4 @@
 class Constants {
-  static const PRODUCT_BASE_URL =
-      'https://shop-cod3r-16b29-default-rtdb.firebaseio.com/products';
-  static const ORDER_BASE_URL =
-      'https://shop-cod3r-16b29-default-rtdb.firebaseio.com/orders';
+  static const PRODUCT_BASE_URL = 'https://shop-cronos-default-rtdb.firebaseio.com/products';
+  static const ORDER_BASE_URL = 'https://shop-cronos-default-rtdb.firebaseio.com/orders';
 }
